9619cd8427eececc54a6d745b899c66459590e84
Polaris Astrum <polaris.astrum@nhs.net>
2023-08-11 09:48:39 +0100

    Corrected slack message content

--- bin/slack_handler.py
+++ bin/slack_handler.py
@@ -12,7 +12,7 @@ class SlackHandler:
     def announce_clinvar_update(self, channel, clinvar_date, month):
         # TODO: learn how to use slack API
         update_message = """
-        The new version of the ClinVar annotation resource file clinvar_{}.vcf.gz ({}) has been deployed into 001_reference
+        The new version of the ClinVar annotation resource file {} ({}) has been deployed into 001_reference
         """.format(clinvar_date, month)
         self.send_message(channel, update_message)
 
