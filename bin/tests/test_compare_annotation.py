0d55c37667741eff81ff3d14865bec2053f912f2
Polaris Astrum <polaris.astrum@nhs.net>
2023-08-16 14:52:05 +0100

    Made unit tests PEP8 compliant

--- bin/tests/test_compare_annotation.py
+++ bin/tests/test_compare_annotation.py
@@ -1,22 +1,26 @@
-from bin.compare_annotation import get_full_category_name
+from bin.compare_annotation import get_full_category_name as get_fc
 
 import os
 os.chdir("..")
 
+
 def test_get_full_category_name():
     # all simple cases
-    assert get_full_category_name("Benign", ".") == "benign"
-    assert get_full_category_name("Benign/Likely_benign", ".") == "benign/likely benign"
-    assert get_full_category_name("not_provided", ".") == "not provided"
-    assert get_full_category_name("Pathogenic", ".") == "pathogenic"
-    assert get_full_category_name("Uncertain_significance", ".") == "uncertain significance"
-    assert get_full_category_name("Pathogenic/Likely_pathogenic", ".") == "pathogenic/likely pathogenic"
-    assert get_full_category_name("risk_factor", ".") == "risk factor"
+    assert get_fc("Benign", ".") == "benign"
+    assert get_fc("Benign/Likely_benign", ".") == "benign/likely benign"
+    assert get_fc("not_provided", ".") == "not provided"
+    assert get_fc("Pathogenic", ".") == "pathogenic"
+    assert get_fc("Uncertain_significance", ".") == "uncertain significance"
+    assert (get_fc("Pathogenic/Likely_pathogenic", ".")
+            == "pathogenic/likely pathogenic")
+    assert get_fc("risk_factor", ".") == "risk factor"
 
     # complex cases
-    assert get_full_category_name("Conflicting_interpretations_of_pathogenicity", ".") != "conflicting interpretations of pathogenicity"
-    assert get_full_category_name("Conflicting_interpretations_of_pathogenicity&other", ".") != "conflicting interpretations of pathogenicity and other"
+    assert (get_fc("Conflicting_interpretations_of_pathogenicity", ".")
+            != "conflicting interpretations of pathogenicity")
+    assert (get_fc("Conflicting_interpretations_of_pathogenicity&other", ".")
+            != "conflicting interpretations of pathogenicity and other")
+
 
 if __name__ == "__main__":
     test_get_full_category_name()
-    
