a94c4bbb016b3c04df2aed199feb403259b64863
Polaris Astrum <polaris.astrum@nhs.net>
2023-08-18 09:38:37 +0100

    Made get_diff.sh comment more clear

--- bin/get_diff.sh
+++ bin/get_diff.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# takes the diff of two input files (input_file_1 and input_file_2)
-# returns a text file containing the output of the bash command "diff"
+# takes the diff of two input files (paths to input_file_1 and input_file_2)
+# returns path the of a text file containing the output of the command "diff"
 # inputs: input_file_1 (string), input_file_2 (string)
-# output: output_file
+# output: output_file (string)
 
 input_file_1=$1
 input_file_2=$2
