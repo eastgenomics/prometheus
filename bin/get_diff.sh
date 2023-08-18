4ba26dec21d9aef9ad3f499980d185dbf8a3f886
Polaris Astrum <polaris.astrum@nhs.net>
2023-08-18 09:35:07 +0100

    Added description comment to get_diff.sh

--- bin/get_diff.sh
+++ bin/get_diff.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# takes the diff of two input files (input_file_1 and input_file_2)
+# returns a text file containing the output of the bash command "diff"
+# inputs: input_file_1 (string), input_file_2 (string)
+# output: output_file
 
 input_file_1=$1
 input_file_2=$2
