f6f79f7c9c4be50ad952c7176223602e8d6a384d
Polaris Astrum <polaris.astrum@nhs.net>
2023-10-03 14:57:36 +0100

    Added features to git_handler and developed vep_config_update

--- bin/git_handler.py
+++ bin/git_handler.py
@@ -2,21 +2,51 @@
 """
 
 from git import Repo
+import subprocess
 
 
 class GitHandler:
-    def __init__(self, repo_directory):
+    def __init__(self, repo_directory, remote_repo_url, branch_name):
         self.repo = Repo.init(repo_directory, bare=True)
+        self.origin = self.repo.create_remote("origin",
+                                              remote_repo_url)
+        # Setup a local tracking branch of a remote branch
+        # create local branch branch_name from remote branch_name
+        self.repo.create_head(branch_name, self.origin.refs[branch_name])
+        # set local branch_name to track remote branch_name
+        (self.repo.heads[branch_name]
+         .set_tracking_branch(self.origin.refs[branch_name]))
+        # checkout local branch_name to working tree
+        self.repo.heads[branch_name].checkout()
 
-    def clone_repo(self, url):
-        # clone remote repo to manage locally
-        pass
+    def pull_repo(self):
+        # pull remote repo
+        self.origin.pull()
 
-    def rename_file(self, old_name, new_name):
-        pass
+    def rename_file(self, folder, old_name, new_name):
+        mv_input = ["git",
+                    "mv"
+                    "{}/{}".format(folder, old_name),
+                    "{}/{}".format(folder, new_name)]
+        subprocess.run(mv_input, stderr=subprocess.STDOUT)
 
     def push_to_remote(self):
-        pass
+        self.origin.push()
+
+    def add_file(self, file_name):
+        self.repo.index.add([file_name])
 
     def commit_changes(self, commit_message):
+        self.repo.index.commit(commit_message)
+
+    def make_branch(self):
+        pass
+
+    def switch_branch(self):
+        pass
+
+    def make_pull_request(self, branch_name):
+        pass
+
+    def merge_pull_request(self, branch_name, merge_into_branch):
         pass
--- bin/vep_config_update.py
+++ bin/vep_config_update.py
@@ -6,6 +6,9 @@ new clinvar updates come in
 import json
 import logging
 import dxpy
+import os
+import re
+import glob
 
 from get_clinvar_files import get_ftp_files, retrieve_clinvar_files
 import make_vep_test_configs as vep
@@ -15,6 +18,7 @@ from login_handler import LoginHandler
 from slack_handler import SlackHandler
 from progress_tracker import ClinvarProgressTracker as Tracker
 from utils import get_prod_version, load_config, check_proj_folder_exists
+from git_handler import GitHandler
 
 logger = logging.getLogger("main log")
 
@@ -52,13 +56,51 @@ def run_vep_config_update(bin_folder, assay, genome_build):
     # push repo to github
     # create pull request
     # merge pull request to main branch
-    pass
+    repo_dir = "temp/vep_repo_{}".format(assay)
+    remote_url = "https://github.com/org/repo_name"
+    invalid_test_url = "invalid"
+    git_handler = GitHandler(repo_dir, invalid_test_url, "main")
+    git_handler.pull_repo()
+    # switch to new branch
+    branch_name = "prometheus_dev_branch"
+    git_handler.make_branch(branch_name)
+    git_handler.switch_branch(branch_name)
+    # search through pulled dir, get old version, update to new
+    repo_files = os.listdir(repo_dir)
+    old_config = ""
+    new_config = ""
+    new_version = ""
+    for file in repo_files:
+        match = re.match(r".*_vep_config_v(.*).json")
+        if match:
+            old_config = file
+            version = match.group(1)
+            new_version_end = int(version.split(".")[2]) + 1
+            search = r".*_vep_config_v.*\..*\.(.*).json"
+            new_config = re.sub(search, new_version_end, old_config)
+            new_version = re.sub(r".*\..*\.(.*)", new_version_end, version)
+            break
+    if old_config == "":
+        raise Exception("No file matching config regex was found in repo")
+    git_handler.rename_file(old_config, new_config)
+    # TODO: edit file contents to update version and config files
+    # TODO: check old clinvar version is older than new version
+    git_handler.add_file("{}/{}".format(repo_dir, new_config))
+    commit_message = ("Updated clinvar file and index IDs and incremented"
+                      + " version number for"
+                      + " {} config file".format(assay))
+    git_handler.commit_changes(commit_message)
+    git_handler.make_pull_request(branch_name)
+    git_handler.merge_pull_request(branch_name, "main")
 
     # clone git repo to get latest config file for given assay
     # upload config file to DNAnexus
     # fetch a valid panel bed and vcf file to run VEP for a given assay
     # run vep using updated config file and record results to 003 project
-    pass
+    git_handler = GitHandler(repo_dir, invalid_test_url, "main")
+    updated_config = glob.glob("temp/*_vep_config_*.json")[0]
+    # TODO: upload to specific 003 test directory
+    #dxpy.upload_local_file(updated_config)
 
     # test results automatically to ensure results are valid
     # output human readable evidence to 003 project
