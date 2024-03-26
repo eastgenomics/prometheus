3228f23a550759d8c7e8e85fea8f2e04ef8650dd
Polaris Astrum <polaris.astrum@nhs.net>
2024-03-26 14:22:05 +0000

    Fixed unit test and improved coverage of test_vep_testing.py

--- tests/test_vep_testing.py
+++ tests/test_vep_testing.py
@@ -6,6 +6,8 @@ sys.path.append(os.path.abspath(
     os.path.join(os.path.realpath(__file__), '../../bin')
 ))
 from bin.util import vep_testing as vt
+import vcfpy
+from collections import OrderedDict
 
 
 class testVepTesting(unittest.TestCase):
@@ -75,16 +77,15 @@ class testVepTesting(unittest.TestCase):
         with self.subTest():
             assert bed == "file-1234512345"
 
-    # TODO: fix package not recognised problem
     @patch("bin.util.vep_testing.dxpy.upload_local_file")
     @patch("bin.util.vep_testing.dxpy.bindings.dxproject.DXProject.new_folder")
     @patch("bin.util.vep_testing.dxpy.bindings.dxproject.DXProject")
     @patch("bin.util.vep_testing.check_proj_folder_exists")
     @patch("bin.util.vep_testing.inspect_logs")
-    @patch("bin.util.vep_testing.dxpy.bindings.dxfile.DXFile.describe")
-    @patch("bin.util.vep_testing.dxpy.bindings.dxfile.DXFile")
+    @patch("bin.util.vep_testing.DXFile.describe")
+    @patch("bin.util.vep_testing.DXFile")
     @patch("bin.util.vep_testing.dxpy.bindings.dxjob.DXJob.get_id")
-    @patch("bin.util.vep_testing.dxpy.bindings.dxjob.DXJob.DXJob.wait_on_done")
+    @patch("bin.util.vep_testing.dxpy.bindings.dxjob.DXJob.wait_on_done")
     @patch("bin.util.vep_testing.run_vep")
     @patch("bin.util.vep_testing.get_recent_vep_vcf_bed")
     def test_vep_testing_config(
@@ -121,17 +122,24 @@ class testVepTesting(unittest.TestCase):
     ):
         mock_folder.return_value = True
         mock_glob.return_value = ["path"]
-        # TODO: mock vcfpy.Reader and provide contents
-        mock_reader.return_value = None
+        sub = vcfpy.Substitution(type_=None, value="test")
+        info = {"CSQ": ["this|is|a|unit|test"]}
+        records = [
+            vcfpy.Record("1", 10, [1], "A", [sub], None, ["filter"], info),
+            vcfpy.Record("2", 15, [5], "G", [sub], None, ["filter"], info),
+            vcfpy.Record("3", 20, [17], "C", [sub], None, ["filter"], info)
+        ]
+        mock_reader.return_value = records
 
         project_id = "project-1234"
         folder = "folder"
         label = "label"
         update_folder = "update_folder"
         # TODO: check write calls of mocked file written to
-        assert vt.parse_vep_output(
+        output = vt.parse_vep_output(
             project_id, folder, label, update_folder
-        ) is str
+        )
+        assert output == "temp/parsed_vcf_label.txt"
 
     @patch("dxpy.bindings.dxapp.DXApp.run")
     @patch("dxpy.bindings.dxapp.DXApp")
@@ -146,6 +154,36 @@ class testVepTesting(unittest.TestCase):
             project_id, project_folder, config_file, vcf_file, panel_bed_file,
             update_folder
         ) is not None
+
+    @patch("bin.util.vep_testing.compare_annotation")
+    @patch("bin.util.vep_testing.get_diff_output")
+    @patch("bin.util.vep_testing.parse_vep_output")
+    @patch("bin.util.vep_testing.dxpy.bindings.dxjob.DXJob.describe")
+    @patch("bin.util.vep_testing.make_job_report")
+    @patch("bin.util.vep_testing.dxpy.bindings.dxjob.DXJob.wait_on_done")
+    @patch("bin.util.vep_testing.run_vep")
+    @patch("bin.util.vep_testing.get_recent_vep_vcf_bed")
+    def test_vep_testing_annotation(
+        self, mock_recent, mock_run, mock_wait, mock_report, mock_describe,
+        mock_parse, mock_diff, mock_compare
+    ):
+        mock_recent.return_value = ("file-1234", "file-1235")
+        mock_run.return_value.mock_wait.return_value = None
+        mock_recent.return_value = ("file-1237", "file-2237")
+        mock_describe.return_value = {"id": "file-1238"}
+        mock_parse.return_value = "file.txt"
+        mock_diff.return_value = "file2.txt"
+        mock_report.return_value = "report.txt"
+        added_csv = "added.csv"
+        deleted_csv = "deleted.csv"
+        changed_csv = "changed.csv"
+        detailed_csv = "detailed.csv"
+        mock_compare.return_value = (
+            added_csv, deleted_csv, changed_csv, detailed_csv
+        )
+        assert vt.vep_testing_annotation(
+            "", "", "", "", "", "", ""
+        ) == (added_csv, deleted_csv, changed_csv, detailed_csv, "report.txt")
 
 
 if __name__ == "__main__":
